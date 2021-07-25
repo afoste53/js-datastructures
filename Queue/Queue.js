c4a15c7153a63b4d933055b8380bc5b3d7a83139
Gabe <mrginch@gmail.com>
2021-07-25 14:59:14 -0400

    add Queue methods

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -1,15 +1,40 @@
+//FIFO
+
 class Queue {
   constructor() {
     this.queue = [];
   }
 }
 
-// enqueue(data)
+// Adds an element to the end of the queue
+Queue.prototype.enqueue = function (data) {
+  this.queue.push(data);
+};
 
-//dequeue
+// Removes the first element in the queue
+Queue.prototype.dequeue = function () {
+  const dequeued = this.queue[0];
+  this.queue.shift();
+  return dequeued;
+};
 
-//peek
+// Returns the first item in the Queue without removing it
+Queue.prototype.peek = function () {
+  return this.queue[0];
+};
 
-//isempty
+// Checks to see if Queue is empty
+Queue.prototype.isEmpty = function () {
+  return this.queue.length === 0;
+};
 
-//print
+// Prints all elements in queue
+Queue.prototype.print = function () {
+  let str = "";
+  let iter = 0;
+  while (iter < this.queue.length) {
+    str += this.queue[iter] + " ";
+    iter++;
+  }
+  return str;
+};
